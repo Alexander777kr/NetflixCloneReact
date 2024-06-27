9ec16f71688349913347ce90e3f642efad60f522
Aleksandr Kabanskiy <alexander-777@inbox.ru>
2024-06-27 18:25:44 +0300

    Building the Login Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
 import './App.css';
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <HomePage />
+        <LoginPage />
       </div>
     </>
   );
