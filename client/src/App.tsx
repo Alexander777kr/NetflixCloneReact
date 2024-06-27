4f28b97222b99f34117b14049837f81ec61fed7c
Aleksandr Kabanskiy <alexander-777@inbox.ru>
2024-06-27 17:16:43 +0300

    Building the NavBar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,7 +8,7 @@ function App() {
   return (
     <>
       <div>
-        <h1 className="text-amber-600">Hello there</h1>
+        <NavBar />
       </div>
     </>
   );
