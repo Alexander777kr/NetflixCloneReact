a96571a0fe979dd21e6a11373850020f458173ac
Aleksandr Kabanskiy <alexander-777@inbox.ru>
2024-07-01 19:22:12 +0300

    refactoring common endpoints to separate files

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const movies = require("./movies.json");
 const cors = require("cors");
+const { prisma } = require("./db");
 
 const app = express();
 
@@ -10,21 +10,8 @@ app.get("/", (req, res) => {
   return res.send("Hello world");
 });
 
-app.get("/movies/list", (req, res) => {
-  const offset = parseInt(req.query.offset);
-  const from = offset;
-  const to = from + 12;
-  const moviesSubset = [...movies].slice(from, to);
-  setTimeout(() => {
-    return res.json({ movies: moviesSubset, count: movies.length });
-  }, 3000);
-});
-
-app.get("/movie/:id", (req, res) => {
-  const id = req.params.id;
-  const movie = movies.find(m => m.id === id);
-  return res.send(movie);
-});
+app.use("", require("./routes/movies"));
+app.use("/auth", require("./routes/auth"));
 
 app.listen(8081, () => {
   console.log("Now listening on PORT 8081");
