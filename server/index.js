e0bf5d3cc82b9443dcd7cc32b984b0e36c1adcc4
Aleksandr Kabanskiy <alexander-777@inbox.ru>
2024-07-01 20:05:46 +0300

    Validating the user inputs

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const { prisma } = require("./db");
 
 const app = express();
-
+app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
--- server/routes/movies.js
+++ server/routes/movies.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
+const { prisma } = require("../db");
 
 router.get("/movies/list", async (req, res) => {
   const offset = parseInt(req.query.offset);
   const count = await prisma.movie.count();
   const movies = await prisma.movie.findMany({
     take: 12,
-    skip: offset
+    skip: offset,
   });
   return res.json({ movies, count });
 });
@@ -14,8 +15,8 @@ router.get("/movie/:id", async (req, res) => {
   const id = req.params.id;
   const movie = await prisma.movie.findUnique({
     where: {
-      id: parseInt(id)
-    }
+      id: parseInt(id),
+    },
   });
   return res.send(movie);
 });
