ddcf1f620fa0e32b0fe6765f41a72dbdd27d1d55
Aleksandr Kabanskiy <alexander-777@inbox.ru>
2024-07-02 10:32:08 +0300

    Forming and returning a JSON Web Token

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const { check, validationResult } = require('express-validator');
 const { prisma } = require("../db");
 const bcrypt = require("bcrypt");
+const JWT = require("jsonwebtoken");
 
 router.post("/signup", [
   check("email", "Please input a valid email").isEmail(),
@@ -34,10 +35,17 @@ router.post("/signup", [
       email,
       username,
       password: hashedPassword
+    },
+    select: {
+      id: true,
+      username: true,
+      email: true
     }
   });
+  const token = await JWT.sign(newUser, process.env.JSON_WEB_TOKEN_SECRET, { expiresIn: 3600000 });
   res.json({
-    user: newUser
+    user: newUser,
+    token
   });
 
 });
