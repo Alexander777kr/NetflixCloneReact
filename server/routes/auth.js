e340c3b9be044331005c253289d13c453c704cb1
Aleksandr Kabanskiy <alexander-777@inbox.ru>
2024-07-02 10:16:39 +0300

    Saving the user

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -29,7 +29,16 @@ router.post("/signup", [
     });
   }
   const hashedPassword = await bcrypt.hash(password, 10);
-  res.send(hashedPassword);
+  const newUser = await prisma.user.create({
+    data: {
+      email,
+      username,
+      password: hashedPassword
+    }
+  });
+  res.json({
+    user: newUser
+  });
 
 });
 
