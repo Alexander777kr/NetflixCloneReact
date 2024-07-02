a5265b6e8b6b360776f34e9e0def7480f40dabdd
Aleksandr Kabanskiy <alexander-777@inbox.ru>
2024-07-02 11:53:42 +0300

    Setting Up the Form with the useForm hook

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,6 +1,13 @@
 import NavBar from '../components/NavBar';
 import Input from '../components/Input';
 import { useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+type Inputs = {
+  email: string;
+  name: string;
+  password: string;
+};
 
 enum Variant {
   SIGN_UP,
@@ -8,14 +15,26 @@ enum Variant {
 }
 
 export default function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm<Inputs>();
   const [variant, setVariant] = useState(Variant.LOGIN_IN);
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  };
   return (
     <div className="relative bg-black h-screen w-screen bg-opacity-50">
       <NavBar />
       <div className="flex justify-center items-center h-full">
         <div className="bg-black bg-opacity-70 p-16 self-center mt-2 w-full max-w-md rounded-md">
           <h2 className="text-white text-4xl mb-8 font-semibold">Sign In</h2>
-          <form className="flex flex-col gap-4">
+          <form
+            className="flex flex-col gap-4"
+            onSubmit={handleSubmit(onSubmit)}
+          >
             {variant === Variant.SIGN_UP && (
               <Input id="username" type="text" label="Username" />
             )}
