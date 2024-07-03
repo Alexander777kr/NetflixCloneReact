153e3e0b0363da5ae988a1618fbfb9349f50c741
Aleksandr Kabanskiy <alexander-777@inbox.ru>
2024-07-03 13:31:19 +0300

    Purchasing a subscription from the client

--- client/src/pages/PlansPage.tsx
+++ client/src/pages/PlansPage.tsx
@@ -1,11 +1,33 @@
+import { useState } from 'react';
 import PlanCard from '../components/PlanCard';
 import usePlans from '../hooks/usePlans';
+import { useAppSelector } from '../app/hooks';
+import axios from 'axios';
+
+const createSession = async (email: string, priceId: string) => {
+  const response = await axios.post('http://localhost:8081/sub/session', {
+    email,
+    priceId,
+  });
+
+  const { url } = response.data;
+  window.location.href = url;
+};
 
 export default function PlansPage() {
-  const { loading, data, error } = usePlans();
+  const { loading, data } = usePlans();
+  const [selectedSession, setSelectedSession] = useState<null | string>(null);
+  const { user } = useAppSelector((state) => state.user.value);
   if (loading || !data) {
     return <div>Loading...</div>;
   }
+
+  const handleClick = () => {
+    if (user && selectedSession) {
+      createSession(user.email, selectedSession);
+    }
+  };
+
   return (
     <div className="flex items-center h-screen justify-center">
       <div className="w-[600px]">
@@ -13,9 +35,21 @@ export default function PlansPage() {
           Choose a plan that works for you
         </h1>
         <div className="flex mt-4">
-          {data && data.map((plan) => <PlanCard plan={plan} key={plan.id} />)}
+          {data &&
+            data.map((plan) => (
+              <PlanCard
+                plan={plan}
+                key={plan.id}
+                selectedSession={selectedSession}
+                setSelectedSession={setSelectedSession}
+              />
+            ))}
         </div>
-        <button className="rounded bg-red-400 p-3 text-white px-10 mt-3 w-full">
+        <button
+          className="rounded bg-red-400 p-3 text-white px-10 mt-3 w-full"
+          disabled={!selectedSession}
+          onClick={handleClick}
+        >
           Purchase
         </button>
       </div>
